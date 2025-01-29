c25261c94c291b2111a3c1fb21fb587e76d645b1
Shri0504 <patilshrikant00504@gmail.com>
2025-01-29 22:56:18 +0530

    update the dashbord page

--- LOGIN_PROJECT/settings.py
+++ LOGIN_PROJECT/settings.py
@@ -128,8 +128,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'your mail'
-EMAIL_HOST_PASSWORD = 'your app password'
+EMAIL_HOST_USER = 'patilshrikant0504@gmail.com'
+EMAIL_HOST_PASSWORD = 'rdzd knfg frcz fado'
 
 
 AUTH_USER_MODEL = 'login_app.CustomUser'
